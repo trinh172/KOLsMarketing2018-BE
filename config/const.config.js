b2173c43fdf66a6c22f224b68b9799794809c6d7
trinh172 <tuyettrinhaquarius172@gmail.com>
2022-02-18 16:15:49 +0700

    create server

---
+++ config/const.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    postsLimit: 6,
+    DOMAIN_API: 'http://localhost:3000/', //set domain API for local
+    DOMAIN_FE: 'http://localhost:3001/',    // set domain FE for local
+//  DOMAIN_API: 'later',//deploy host API
+//  DOMAIN_FE: 'later',//deploy host frontend
+}
